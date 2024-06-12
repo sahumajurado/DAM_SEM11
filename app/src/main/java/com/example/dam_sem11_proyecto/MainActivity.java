b34662c7d8f7d0d30d7b29fe26da9e277bca7020
sahumajurado <2113110059@untels.edu.pe>
2024-06-12 12:12:45 -0500

    Semana 11 - Done

---
+++ app/src/main/java/com/example/dam_sem11_proyecto/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.dam_sem11_proyecto;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
